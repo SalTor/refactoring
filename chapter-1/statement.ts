54b5926925149196197942649827f7cfb1cea447
SalTor <sal@saltor.nyc>
2024-04-14 16:28:39 -0400

    Rename variables for clarity

--- chapter-1/statement.ts
+++ chapter-1/statement.ts
@@ -17,11 +17,11 @@ export function statement(invoice: Invoice, plays: Plays) {
   return result;
 
   function getTotalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += getVolumeCreditsFor(perf);
+      result += getVolumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 
   function usd(cents: number) {
