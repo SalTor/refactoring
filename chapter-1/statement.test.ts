9111b62aad73a17e9cbe707642d8ea540d5ebf0a
SalTor <sal@saltor.nyc>
2024-04-16 21:43:43 -0400

    Create htmlStaement with test

--- chapter-1/statement.test.ts
+++ chapter-1/statement.test.ts
@@ -1,4 +1,4 @@
-import { statement } from "./statement";
+import { htmStatement, statement } from "./statement";
 import invoices from "./invoices.json";
 import plays from "./plays.json";
 
@@ -12,7 +12,18 @@ You earned 47 credits
 `);
 });
 
-test.todo("Render as html");
+test("Render as html", () => {
+  expect(htmStatement(invoices[0], plays)).toBe(`<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>
+<tr><td>Hamlet</td><td>55</td><tr><td>$650.00</td></tr>
+<tr><td>As You Like It</td><td>35</td><tr><td>$580.00</td></tr>
+<tr><td>Othello</td><td>40</td><tr><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`);
+});
 
 describe("Support additional kinds of plays", () => {
   test.todo("placeholder");
--- chapter-1/statement.ts
+++ chapter-1/statement.ts
@@ -16,12 +16,35 @@ export function renderPlaintext(data: StatementData) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
 
   return result;
+}
 
-  function usd(cents: number) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(cents / 100);
+function usd(cents: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(cents / 100);
+}
+
+export function htmStatement(invoice: Invoice, plays: Plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+export function renderHtml(data: StatementData) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+
+  result += `<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>
+`;
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<tr><td>${usd(perf.amount)}</td></tr>\n`;
   }
+
+  result += "</table>\n";
+
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+
+  return result;
 }
