f9495587f76606f797d7e498030266a9ecadfe90
SalTor <sal@saltor.nyc>
2024-04-20 16:10:09 -0400

    Place structs beneath main code

--- chapter-1/rust/src/main.rs
+++ chapter-1/rust/src/main.rs
@@ -2,18 +2,6 @@ use std::{error::Error, fs::File, io::BufReader, path::Path};
 
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Invoice {
-  customer: String,
-  performances: Vec<Performance>
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Performance {
-  play_id: String,
-  audience: i8,
-}
-
 fn main() {
   let json = read_invoices_from_file("../invoices.json").unwrap();
 
@@ -25,4 +13,16 @@ fn read_invoices_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Invoice>, Box<
     let reader = BufReader::new(file);
 
     Ok(serde_json::from_reader(reader)?)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Invoice {
+  customer: String,
+  performances: Vec<Performance>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Performance {
+  play_id: String,
+  audience: i8,
 }
