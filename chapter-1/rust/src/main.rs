ab4d8f38f7149e48c47cbb01a60b742a4cbc919a
SalTor <sal@saltor.nyc>
2024-04-20 16:19:55 -0400

    Add reading of plays, too

--- chapter-1/rust/src/main.rs
+++ chapter-1/rust/src/main.rs
@@ -1,11 +1,24 @@
-use std::{error::Error, fs::File, io::BufReader, path::Path};
+use std::{collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
 
 use serde::{Serialize, Deserialize};
 
 fn main() {
-  let json = read_invoices_from_file("../invoices.json").unwrap();
+  let invoices = read_invoices_from_file("../invoices.json").unwrap();
+  let plays = read_plays_from_file("../plays.json").unwrap();
 
-  println!("{:?}", json);
+  statement(invoices.into_iter().next().unwrap(), plays);
+}
+
+fn statement(invoice: Invoice, plays: HashMap<String, Play>) {
+  println!("{:?}", invoice);
+  println!("{:?}", plays);
+}
+
+fn read_plays_from_file<P: AsRef<Path>>(path: P) -> Result<HashMap<String, Play>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    Ok(serde_json::from_reader(reader)?)
 }
 
 fn read_invoices_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Invoice>, Box<dyn Error>> {
@@ -25,4 +38,10 @@ struct Invoice {
 struct Performance {
   play_id: String,
   audience: i8,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Play {
+  name: String,
+  r#type: String
 }
