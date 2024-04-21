032bd01976d1a4e06851c693988869e4c8de8bfb
SalTor <sal@saltor.nyc>
2024-04-20 21:03:31 -0400

    Don't render for unsupported play types + format money

--- chapter-1/rust/src/main.rs
+++ chapter-1/rust/src/main.rs
@@ -1,32 +1,65 @@
-use std::{collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
+use std::{cmp, collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
 
-use serde::{Serialize, Deserialize};
+use rusty_money::{iso, Money};
+use serde::{Deserialize, Serialize};
 
 fn main() {
-  let invoices = read_invoices_from_file("../invoices.json").unwrap();
-  let plays = read_plays_from_file("../plays.json").unwrap();
+    let invoices = read_invoices_from_file("../invoices.json").unwrap();
+    let plays = read_plays_from_file("../plays.json").unwrap();
 
-  statement(invoices.into_iter().next().unwrap(), plays);
+    println!("{}", statement(invoices.into_iter().next().unwrap(), plays));
 }
 
-fn statement(invoice: Invoice, plays: HashMap<String, Play>) {
-  println!("{:?}", invoice);
-  println!("{:?}", plays);
+fn statement(invoice: Invoice, plays: HashMap<String, Play>) -> String {
+    let mut total_amount = 0;
+    let mut volume_credits = 0;
+    let mut result = format!(
+        "Statement for {client_name}\n",
+        client_name = invoice.customer
+    );
 
-  let total_amount = 0;
-  let volume_credits = 0;
-  let result = format!("Statement for {client_name}\n", client_name = invoice.customer);
+    println!("{} {} {}", total_amount, volume_credits, result);
 
-  println!("{} {} {}", total_amount, volume_credits, result);
+    for perf in invoice.performances.iter() {
+        if let Some(play) = plays.get(&perf.play_id) {
+            let play_type = play.r#type.to_owned();
+            let this_amount = match play_type.as_str() {
+                "tragedy" => {
+                    let mut amount: i32 = 40000;
+                    if perf.audience > 30 {
+                        amount += 1000 * i32::from(perf.audience - 30);
+                    }
+                    amount
+                }
+                "comedy" => {
+                    let mut amount: i32 = 30000;
+                    if perf.audience > 20 {
+                        amount += 10000 + 500 * i32::from(perf.audience - 20);
+                    }
+                    amount + 300 * i32::from(perf.audience)
+                }
+                _ => continue,
+            };
 
-  for perf in invoice.performances.iter() {
-    let play = plays.get(&perf.play_id);
+            volume_credits += i32::from(cmp::max(perf.audience - 30, 0));
+            if play_type.as_str() == "comedy" {
+                volume_credits += i32::from(perf.audience / 5);
+            }
 
-    match play {
-      Some(play) => println!("{:?}", play),
-      None => println!("Play not found")
+            result += &format!(
+                " {play_name}: {amount} ({seats} seats)\n",
+                play_name = play.name,
+                amount = Money::from_major((this_amount / 100).into(), iso::USD),
+                seats = perf.audience
+            );
+            total_amount += this_amount;
+        };
     }
-  }
+
+    result += &format!("Amount owed is {amount}\n", amount = Money::from_major((total_amount / 100).into(), iso::USD));
+    result += &format!("You earned {credits} credits\n", credits = volume_credits);
+
+    result
 }
 
 fn read_plays_from_file<P: AsRef<Path>>(path: P) -> Result<HashMap<String, Play>, Box<dyn Error>> {
@@ -45,18 +78,18 @@ fn read_invoices_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Invoice>, Box<
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Invoice {
-  customer: String,
-  performances: Vec<Performance>
+    customer: String,
+    performances: Vec<Performance>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Performance {
-  play_id: String,
-  audience: i8,
+    play_id: String,
+    audience: i8,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Play {
-  name: String,
-  r#type: String
+    name: String,
+    r#type: String,
 }
